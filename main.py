b88c4cc8b6590bbfff5ab41d800653f014a6d627
erlyg <erlyginegor@gmail.com>
2025-10-26 15:11:04 +0300

    Сделаны поля для ввода и вывода

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QFont
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLineEdit
 
 
 class Advanced_Calculator(QMainWindow):
@@ -8,8 +11,26 @@ class Advanced_Calculator(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(400, 200, 400, 600)
+        self.setGeometry(450, 150, 500, 650)
         self.setWindowTitle('Калькулятор')
+
+        # ------------------ основное поле ввода (нижнее) -----------------
+        self.main_label = QLineEdit(self)
+        self.main_label.setText('0')
+        self.main_label.setReadOnly(True)
+        self.main_label.move(0, 50)
+        self.main_label.resize(500, 100)
+        font = QFont("Arial", 30)
+        self.main_label.setFont(font)
+        self.main_label.setAlignment(Qt.AlignmentFlag.AlignRight)
+
+        # -----------------дополнительное поле (верхнее)---------------------
+
+        self.secondary_label = QLineEdit(self)
+        self.secondary_label.setReadOnly(True)
+        self.secondary_label.move(0, 50)
+        self.secondary_label.resize(500, 20)
+        self.secondary_label.setAlignment(Qt.AlignmentFlag.AlignRight)
 
 
 if __name__ == '__main__':
