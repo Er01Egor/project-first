f0726206a634c99e3d60168c7fd33f87be6ccdcb
erlyg <erlyginegor@gmail.com>
2025-10-26 11:15:47 +0300

    Другая основа программы (с QWidget на QMainWindow).

--- main.py
+++ main.py
@@ -1,29 +1,19 @@
-import io
 import sys
-
-from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLineEdit, QPushButton, QListWidget
-from PyQt6.QtCore import QObject
-
-template = """
-"""
+from PyQt6.QtWidgets import QApplication, QMainWindow
 
 
 class Advanced_Calculator(QMainWindow):
     def __init__(self):
         super().__init__()
-        f = io.StringIO(template)
-        uic.loadUi(f, self)
+        self.initUI()
 
-
-
-def except_hook(cls, exception, traceback):
-    sys.__excepthook__(cls, exception, traceback)
+    def initUI(self):
+        self.setGeometry(400, 200, 400, 600)
+        self.setWindowTitle('Калькулятор')
 
 
 if __name__ == '__main__':
-    sys.excepthook = except_hook
     app = QApplication(sys.argv)
-    ex = Advanced_Calculator()
-    ex.show()
+    calculator = Advanced_Calculator()
+    calculator.show()
     sys.exit(app.exec())
