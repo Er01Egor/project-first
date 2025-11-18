1e32c83d81639bb9364a5f7aa7789daec87033c4
erlyg <erlyginegor@gmail.com>
2025-11-18 20:11:03 +0300

    Стиль приложения

---
+++ styles.py
@@ -0,0 +1,12 @@
+style = """
+                QMainWindow {
+                    background-color: #1d1c21;
+                }
+                QPushButton {
+                    background-color: #4f18e7;
+                    color: white;
+                }
+                QPushButton:hover {
+                    background-color: #5d3eb1;
+                }
+            """
